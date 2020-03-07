e89ea61a2513e4726c2cab90a0e042eaef5fcabb
yckj0834 <lixueping@cloudwalk.cn>
2020-03-08 05:37:58 +0800

    auto Sun Mar  8 05:37:58 CST 2020

---
+++ Makefile
@@ -0,0 +1,22 @@
+.PHONY: push pull install run clean
+
+push:
+	git add .
+	git commit -m "auto `date`"
+	git push origin $(shell git branch|grep '*'|awk '{print $$2}')
+
+pull:
+	git pull origin $(shell git branch|grep '*'|awk '{print $$2}')
+
+install: Makefile main.go
+	go build
+	chmod +x message
+	./message
+
+run: main.go
+	go run main.go static
+
+clean:
+	rm -f 123.mp4
+	rm -f 1.db
+
--- executors/httpstatic/httpstatic.go
+++ executors/httpstatic/httpstatic.go
@@ -1,6 +1,7 @@
 package httpstatic
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jroimartin/gocui"
 	"github.com/lflxp/showme/utils"
 )
@@ -22,14 +24,15 @@ var (
 	initnum   int
 )
 
+const maxUploadSize = 2000 * 1024 * 2014 // 2 MB
+const uploadPath = "/tmp"
+
 func init() {
 	initnum = 0
 	// path = utils.GetCurrentDirectory()
 	// port = "9090"
 	closeChan = make(chan os.Signal)
 }
-
-type HandlerFunc func(http.ResponseWriter, *http.Request)
 
 func DecorderHandler(h http.Handler, g *gocui.Gui) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -39,18 +42,81 @@ func DecorderHandler(h http.Handler, g *gocui.Gui) http.Handler {
 	})
 }
 
-func server(g *gocui.Gui) {
-	if initnum == 0 {
-		fileHandler := http.FileServer(http.Dir(path))
-		http.Handle("/", DecorderHandler(fileHandler, g))
-		initnum++
-	}
+// 跨域设置
+func Cors(g *gocui.Gui) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		v, _ := g.View("history")
+		fmt.Fprintln(v, fmt.Sprintf("%s - %s - %s - http://%s%s", time.Now().Format("2006-01-02 15:04:05"), c.Request.RemoteAddr, c.Request.Method, c.Request.Host, c.Request.RequestURI))
 
-	// http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		var headerKeys []string
+		for k := range c.Request.Header {
+			headerKeys = append(headerKeys, k)
+		}
+		// headerStr := strings.Join(headerKeys, ", ")
+		// if headerStr != "" {
+		// 	headerStr = fmt.Sprintf("access-control-allow-origin, access-control-allow-headers, %s", headerStr)
+		// } else {
+		// 	headerStr = "access-control-allow-origin, access-control-allow-headers"
+		// }
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Header("Access-Control-Allow-Origin", "*")                                       // 这是允许访问所有域
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE") //服务器支持的所有跨域请求的方法,为了避免浏览次请求的多次'预检'请求
+			//  header的类型
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session,X_Requested_With,Accept, Origin, Host, Connection, Accept-Encoding, Accept-Language,DNT, X-CustomHeader, Keep-Alive, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Pragma")
+			//              允许跨域设置                                                                                                      可以返回其他子段
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar") // 跨域关键设置 让浏览器可以解析
+			c.Header("Access-Control-Max-Age", "172800")                                                                                                                                                           // 缓存请求信息 单位为秒
+			c.Header("Access-Control-Allow-Credentials", "false")                                                                                                                                                  //  跨域请求是否需要带cookie信息 默认设置为true
+			c.Set("content-type", "application/json")                                                                                                                                                              // 设置返回格式是json
+		}
+
+		//放行所有OPTIONS方法
+		//if method == "OPTIONS" {
+		//    c.JSON(http.StatusOK, "Options Request!")
+		//}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		// 处理请求
+		c.Next() //  处理请求
+	}
+}
+
+func serverGin(g *gocui.Gui) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	router.Use(Cors(g))
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return ""
+	}))
+	// 使用 Recovery 中间件
+	router.Use(gin.Recovery())
+
+	router.StaticFS("/", http.Dir(path))
+	// curl -X POST http://127.0.0.1:9090/upload -F "file=@/Users/lxp/123.mp4" -H "Content-Type:multipart/form-data"
+	router.POST("/upload", func(c *gin.Context) {
+		// 多文件
+		form, _ := c.MultipartForm()
+		files := form.File["file"]
+
+		for _, file := range files {
+
+			// 上传文件到指定的路径
+			c.SaveUploadedFile(file, fmt.Sprintf("./%s", file.Filename))
+		}
+		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+	})
+
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
-		Handler: http.DefaultServeMux,
+		Handler: router,
 	}
+
 	signal.Notify(closeChan,
 		syscall.SIGINT,
 		syscall.SIGTERM,
@@ -58,13 +124,26 @@ func server(g *gocui.Gui) {
 		os.Interrupt,
 		os.Kill,
 	)
+
 	go func() {
 		<-closeChan
-		server.Close()
-		// server.RegisterOnShutdown(func() { return })
-		fmt.Println(utils.Colorize("quit", "red", "", false, false))
+		log.Println("Shutdown Server ...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatal("Server Shutdown:", err)
+		}
+		log.Println("Server exiting")
 	}()
-	go server.ListenAndServe()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpect", err.Error())
+		}
+	}
 }
 
 func HttpStaticServeForCorba(ports, paths string) {
@@ -196,7 +275,8 @@ func dlayout(g *gocui.Gui) error {
 		// fmt.Fprintf(v, time.Now().Format("2006-01-02 15:04:05"))
 		// uri = fmt.Sprintf("/a%s", time.Now().Format("150405"))
 	}
-	if v, err := g.SetView("top", maxX/2-60, maxY/2, maxX/2+60, maxY/2+3); err != nil {
+	ips := utils.GetIPs()
+	if v, err := g.SetView("top", maxX/2-80, maxY/2, maxX/2+80, maxY/2+2*len(ips)+4); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -209,8 +289,21 @@ func dlayout(g *gocui.Gui) error {
 		// v.Editable = true
 		// fmt.Fprintf(v, time.Now().Format("2006-01-02 15:04:05"))
 		// uri = fmt.Sprintf("/a%s", time.Now().Format("150405"))
-		fmt.Fprintln(v, fmt.Sprintf("URL => 0.0.0.0:%s <= \nPATH: => %s <=", port, path))
-		go server(g)
+
+		urls := []string{}
+
+		for _, ip := range ips {
+			urls = append(urls, fmt.Sprintf("UploadURL: => %s:%s <= PATH: => /upload <= ", ip, port))
+		}
+		for _, ip := range ips {
+			urls = append(urls, fmt.Sprintf("DownURL: => %s:%s <= PATH: => / <= ", ip, port))
+		}
+		dir, _ := os.Getwd()
+		urls = append(urls, fmt.Sprintf("CurrentPWD: => %s <= ", dir))
+		urls = append(urls, fmt.Sprintf("UploadDIR: => %s <= ", dir))
+		urls = append(urls, "curl -X POST http://127.0.0.1:9090/upload -F \"file=@/Users/lxp/123.mp4\" -H \"Content-Type:multipart/form-data\"")
+		fmt.Fprintln(v, strings.Join(urls, "\n"))
+		go serverGin(g)
 
 		if _, err = setCurrentViewOnTop(g, "top"); err != nil {
 			return err
